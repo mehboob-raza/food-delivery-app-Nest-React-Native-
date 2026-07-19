8dc1a01474fa2304841db5887937eff8a4c2f805
unknown <razamehboob61@gmail.com>
2026-07-20 02:40:02 +0300

    solve types errors

--- apps/api/src/app.controller.ts
+++ apps/api/src/app.controller.ts
@@ -1,24 +1,23 @@
 import { Controller, Get, Inject } from '@nestjs/common';
 import type { HealthCheckResponse } from '@food-delivery/types';
-import * as schema from './db/schema'
 import { NeonHttpDatabase } from 'drizzle-orm/neon-http';
+import * as schema from './db/schema';
+
 @Controller()
 export class AppController {
-  constructor(@Inject('DB') private db: NeonHttpDatabase<typeof schema>) { }
+  constructor(@Inject('DB') private db: NeonHttpDatabase<typeof schema>) {}
 
   @Get('db-test')
-  async dbTest(){
-    const result = await this.db.select().from(schema.users)
-    return {users : result, count : result.length}
+  async dbTest() {
+    const result = await this.db.select().from(schema.users);
+    return { users: result, count: result.length };
   }
 
-
-  @Get('health')
+  @Get('health') // /api/health
   health(): HealthCheckResponse {
     return {
       status: 'ok',
-      timestamp: new Date()
-    }
+      timestamp: new Date(),
+    };
   }
-
 }
--- apps/api/src/db/db.module.ts
+++ apps/api/src/db/db.module.ts
@@ -5,15 +5,15 @@ import * as schema from './schema';
 
 @Global()
 @Module({
-    providers: [
-        {
-            provide: 'DB',
-            useFactory: () => {
-                const sql = neon(process.env.DATABASE_URL!);
-                return drizzle(sql , { schema });
-            },
-        },
-    ],
-    exports: ['DB'],
+  providers: [
+    {
+      provide: 'DB',
+      useFactory: () => {
+        const sql = neon(process.env.DATABASE_URL!);
+        return drizzle(sql, { schema });
+      },
+    },
+  ],
+  exports: ['DB'],
 })
-export class DbModule { }
+export class DbModule {}
